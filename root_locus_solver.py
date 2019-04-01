6bdd1c98596ed76480102d887009eb8555a50e3d
Aristos Athens <aristos.a.athens@gmail.com>
2019-04-01 13:38:47 -0700

    Minor comment changes.

--- root_locus_solver.py
+++ root_locus_solver.py
@@ -1,12 +1,6 @@
 '''
     Aristos Athens
     Root Locus Solver
-
-    Example:
-        from root_locus_solver import root_locus
-        poles = [ -4 + 2j, -4 - 2j, 1 ]
-        zeros = [ -1 ]
-        values = root_locus(poles = poles, zeros = zeros)
 '''
 
 __all__ = [ "root_locus" ]
